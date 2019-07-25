7002c1ecadd8b3e1703eadc8bba3ea660b25a6a7
Tan Ho <hodinhtan@gmail.com>
2019-07-25 20:28:45 +0700

    chapter 8-9-10 skimming

---
+++ system-call/itimer.c
@@ -0,0 +1,35 @@
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+
+void timer_handler (int signum)
+{
+  static int count = 0;
+  printf ("timer expried %d times \n", ++ count);
+}
+
+int main ()
+{
+  struct sigaction sa;
+  struct itimerval timer;
+
+  // Install timer_handler as the signal handler for SIGVTALRM
+  memset (&sa, 0, sizeof (sa));
+  sa.sa_handler = &timer_handler;
+  sigaction (SIGVTALRM, &sa, NULL);
+
+  // configure the timer to expire after 250msec ...
+  timer.it_value.tv_sec = 0;
+  timer.it_value.tv_usec = 250000;
+  // .. and every 250 msec after that 
+  timer.it_interval.tv_sec = 0;
+  timer.it_interval.tv_usec = 250000;
+
+  // start a virtual timer. It counts down whenever this process is executing 
+  setitimer (ITIMER_VIRTUAL, &timer, NULL);
+
+  // do busy work;
+  while (1);
+
+}
